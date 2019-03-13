ba5c7f43602ddc967068b6d8e63b92a1afc26cf8
peter-keller <keller.f.peter@gmail.com>
2019-03-13 10:49:10 +0000

    working letter verification

--- methods.rb
+++ methods.rb
@@ -2,14 +2,16 @@
 
 require 'random-word'
 
+
+
 # Returns one random word
 def getWord
     return RandomWord.adjs.next
 end
 
-$word = getWord
-
-
+@word = getWord()
+puts @word
+@guess = "_" * @word.length
 # Prompts user for an input and calls the checkLetter function
 def getInput
     puts "Guess a letter:"
@@ -18,11 +20,12 @@ end
 
 # Receives a letter and checks if in random word
 def checkLetter(letter)
-    $word.each do |let|
-        if let == let
-            puts "Hurray"
+    @word.split("").each_with_index do |item, index|
+        if item == letter
+            @guess[index] = letter
         end
     end
+    puts @guess
 end
 
 # Display underscores at first and then changes when letter has been guessed
