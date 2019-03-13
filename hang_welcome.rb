25cb7ed93108c233a2df1a0bf6a41a9cc61509f9
oli-gra <oliver.grant@gmail.com>
2019-03-13 10:24:09 +0000

    Welcome text + run file

---
+++ hang_welcome.rb
@@ -0,0 +1,4 @@
+puts "Welcome to the sometimes fatal guessing game HANGMAN"
+puts ""
+puts "Letters are empty, you guess any alpha, right you're closer to that elusive word, wrong you're on the way to the gallows"
+puts ""
---
+++ run.rb
@@ -0,0 +1,8 @@
+require 'methods'
+require 'hang_welcome'
+
+def run
+	hang_welcome
+end
+
+run
