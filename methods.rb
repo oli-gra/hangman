d014829c4effd4b37830dff8f1f078e041f00400
peter-keller <keller.f.peter@gmail.com>
2019-03-13 10:35:20 +0000

    -

--- methods.rb
+++ methods.rb
@@ -2,25 +2,36 @@
 
 require 'random-word'
 
-
-
+# Returns one random word
 def getWord
     return RandomWord.adjs.next
 end
 
+$word = getWord
+
+
+# Prompts user for an input and calls the checkLetter function
 def getInput
     puts "Guess a letter:"
     checkLetter(gets.chomp)
 end
 
+# Receives a letter and checks if in random word
 def checkLetter(letter)
-    puts letter
+    $word.each do |let|
+        if let == let
+            puts "Hurray"
+        end
+    end
 end
 
+# Display underscores at first and then changes when letter has been guessed
 def displayLetter(str)
     underscore = "_"
     puts "_" * str.length
 end
 
 # puts getWord
-displayLetter(getWord)
+#displayLetter(getWord)
+
+getInput
